3510e6c46194d6b3fe5d5deca7734fad44c840d3
Yux <128932281+YakuzaSan@users.noreply.github.com>
2024-10-08 08:39:28 +0200

    Update route.ts

--- app/api/facts/route.ts
+++ app/api/facts/route.ts
@@ -2,9 +2,12 @@ import { NextResponse } from "next/server";
 export const revalidate = 0
 export async function GET() {
     const NAME_ANIME_GAME_FACTS: Array<string> = [
-         "WHYYYYYYYYYYYYYYYYYYYY",
-        "OZAN DU KLEINER BASTARD",
-         "nzhdeh",
+"    Infektionen: Bakterien (z.B. E. coli, Salmonellen), Viren (z.B. Norovirus, Rotavirus), Parasiten (z.B. Giardia).",
+"    Lebensmittelvergiftung: Verzehr kontaminierter Lebensmittel oder Wasser.",
+"    Medikamente: Antibiotika können das Gleichgewicht der Darmflora stören.",
+"    Nahrungsmittelunverträglichkeiten: Laktoseintoleranz, Glutenunverträglichkeit (Zöliakie).",
+"    Erkrankungen: Reizdarmsyndrom (IBS), entzündliche Darmerkrankungen (z.B. Morbus Crohn, Colitis ulcerosa).",
+"    Stress und Angst: Emotionale Faktoren können den Darm beeinflussen.",
 
     ];
     let randomFact = NAME_ANIME_GAME_FACTS[Math.floor(Math.random() * NAME_ANIME_GAME_FACTS.length + 1)];
