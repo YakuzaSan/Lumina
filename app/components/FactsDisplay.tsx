5e3b19a8a97d3dfdb4f898188dee5cf7b58b684d
Yux <128932281+YakuzaSan@users.noreply.github.com>
2024-10-08 08:39:57 +0200

    Update FactsDisplay.tsx

--- app/components/FactsDisplay.tsx
+++ app/components/FactsDisplay.tsx
@@ -6,7 +6,7 @@ export default function FactDisplay({ fact }: { fact: string }) {
             <h2
                 className={`font-bold text-[1rem] lg:text-[1.25rem] text-[#D1D1D1]`}
             >
-                Fact for you
+                Fact for you about diarrhea
             </h2>
             <div
                 className={`flex justify-center items-center text-[1.1rem] bg-[#222222] text-[#D1D1D1] text-center rounded-lg py-2 px-5`}
